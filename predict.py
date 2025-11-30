7c9d4ba2083ea547046582078b064b42b096d42f
Joshua Wells <joshwells637@gmail.com>
2025-11-30 11:38:59 -0500

    working on error analysis

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 import os
+import cv2
 import glob
 
 def predict():
@@ -19,22 +20,41 @@ def predict():
 
     detections_found = 0
 
-    # Loop through the first 20 images (or all of them if you prefer)
     for i, image_path in enumerate(image_files):
         
-        # Lower confidence to 0.25 to catch obscure guns
-        results = model.predict(image_path, conf=0.25, verbose=False)
+        results = model.predict(image_path, conf=0.40, verbose=False)
         result = results[0]
 
         # If boxes are detected (len(result.boxes) > 0)
         if len(result.boxes) > 0:
-            detections_found += 1
+            img = result.orig_img.copy()
+
+            for box in result.boxes:
+                x1, y1, x2, y2 = box.xyxy[0].cpu().numpy().astype(int)
+                
+                box_color = (0, 255, 0)  # Green
+                thickness = 3
+                
+                cls_id = int(box.cls[0])
+                class_name = model.names[cls_id]
+
+                cv2.rectangle(img, (x1, y1), (x2, y2), box_color, thickness)
+
+                conf = float(box.conf[0])
+                label = f"{class_name}: {conf:.2f}"
+                
+                t_size = cv2.getTextSize(label, 0, fontScale=0.6, thickness=1)[0]
+                cv2.rectangle(img, (x1, y1 - t_size[1] - 4), (x1 + t_size[0], y1), box_color, -1)
+                
+                cv2.putText(img, label, (x1, y1 - 4), 0, 0.6, (0, 0, 0), 2)
+
             filename = os.path.basename(image_path)
-            save_path = os.path.join(output_dir, f"detected_{filename}")
-            
-            # Save the image with drawn boxes
-            result.save(filename=save_path)
-            print(f"  -> Gun detected in {filename}! Saved to {output_dir}")
+            save_path = os.path.join(output_dir, f"{filename}")
+            cv2.imwrite(save_path, img)
+
+            print(f"  -> Gun detected, saved finding to {filename}")
+            detections_found +=1
+        
     
     print(f"\nScan complete. Found firearms in {detections_found} images.")
     print(f"Check the '{output_dir}' folder to see them.")
