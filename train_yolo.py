928e2250c56c03b80f6e14a745ca64ca9ff88af3
Joshua Wells <joshwells637@gmail.com>
2025-12-02 23:19:12 -0500

    current work

--- train_yolo.py
+++ train_yolo.py
@@ -2,15 +2,15 @@ from ultralytics import YOLO
 
 def main():
     print("Loading YOLOv8-Nano model...")
-    model = YOLO('yolov8n.pt') 
+    model = YOLO('yolov8n.pt') # trying the medium model
 
     data_yaml_path = "./datasets/yolo_dataset/dataset.yaml"
 
     print("Starting training...")
     results = model.train(
         data=data_yaml_path,
-        epochs=15,             
-        imgsz=640,            
+        epochs=15, # yolo has early stopping, so this shouldn't overtrain             
+        imgsz=1280, # with higher resolution images!            
         batch=16,              
         name='firearm_detector', # Name of the sub-folder where results will be saved
         device='0',             # Use '0' for GPU, or 'cpu' if you don't have CUDA
